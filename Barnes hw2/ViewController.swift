6d58965b4a1191e4c5f58facbd8bed91b8dfc9c2
Ben Barnes <bm.barnes@setonhill.edu>
2020-02-14 20:12:48 -0500

    Initial Commit

---
+++ Barnes hw2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Barnes hw2
+//
+//  Created by ben barnes on 2/14/20.
+//  Copyright © 2020 ben barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
